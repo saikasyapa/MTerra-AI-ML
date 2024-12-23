d4952b06567f85acc947cb84fa11f0b1e8350316
sai kasyapa <sai.kasyapa1919@gmail.com>
2024-12-23 18:53:30 +0530

    updated all python scripts that log need to save in s3 bucket

--- Stage-2-DataSetcreation/LINE.py
+++ Stage-2-DataSetcreation/LINE.py
@@ -2,16 +2,27 @@
 # line_mask_generator.py
 import logging
 import os.path
-from common import (read_zip, generate_line_mask, get_masked, TARGET_SHAPE, IN_DIR, S3_BUCKET_NAME, S3_MAIN_DIR
-                , upload_image_to_s3, s3_client)
+from common import (read_zip, generate_line_mask, get_masked, TARGET_SHAPE, IN_DIR, S3_BUCKET_NAME, S3_MAIN_DIR, upload_image_to_s3, s3_client)
+from tempfile import gettempdir
 
 # S3 Configuration
 S3_SUB_DIR = "retrain_data/line/label"
-S3_LOG_DIR = "retrain_data/logs/line"
+S3_LOG_DIR = "logs"
 
-log_file_path = "/tmp/line_mask_generator.log"
-logging.basicConfig(level=logging.INFO, format='%(asctime)s;%(levelname)s;%(message)s',
-                    handlers=[logging.FileHandler(log_file_path)])
+# Get a cross-platform temporary directory and define lof file path
+temp_dir = gettempdir() # This dynamically retrieves the temp directory (e.g., /tmp on Linux, C:\Temp on Windows)
+log_file_path = os.path.join(temp_dir, "line_mask_generator.log")
+
+# Ensure the directory for the log file exists
+os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
+# Set up logging to a local file
+file_handler = logging.FileHandler(log_file_path)
+stream_handler = logging.StreamHandler()
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s;%(levelname)s;%(message)s',
+    handlers=[file_handler, stream_handler]
+)
 
 def generate_lines_from_json(obs, sketch_name, image_shape, attachment):
     lines, points_dict = obs['lines'], obs['points']
@@ -58,18 +69,23 @@ if __name__ == '__main__':
     except Exception as e:
         logging.error(f"Error listing or processing ZIP files from S3: {e}")
 
-    # Upload the log file to S# after processing is done
+    # Upload the log file to S3 after processing is done
     try:
-        s3_log_key=f"{S3_MAIN_DIR}/{S3_LOG_DIR}/line_mask_generator.log"
+        s3_log_key=f"{S3_MAIN_DIR}/{S3_LOG_DIR}/line_mask_generator.txt"
         logging.info(f"Uploading log file to S3: {s3_log_key}")
+        
         # Upload the log file to S3
         with open(log_file_path, 'rb') as log_file:
-            s3_client.put_object(Body=log_file, Bucket=S3_BUCKET_NAME, key=s3_log_key)
+            s3_client.put_object(Body=log_file, Bucket=S3_BUCKET_NAME, Key=s3_log_key)
         logging.info("log file uploaded successfully.")
+    
     except Exception as e:
         logging.error(f"Error uploading log file to S3: {e}")
+    
     finally:
-        #Clean up the lof file
+        # Close the logging file handler
+        file_handler.close()
+        logging.getLogger().removeHandler(file_handler)
         if os.path.exists(log_file_path):
             os.remove(log_file_path)
             logging.info("Cleaned up the local log file.")
